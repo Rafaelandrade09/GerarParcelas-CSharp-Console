99cc3afb1360be2d2f8cbc10848951ec33b83449
Rafael Andrade <rafael.andrade09@outlook.com.br>
2021-11-10 23:17:31 -0300

    Criado - Gerar valores de parcelas com base na regra de negocios

---
+++ _14Exerc/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+using _14Exerc.Entities;
+using _14Exerc.Services;
+
+namespace _14Exerc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter contract data ");
+            Console.Write("Number: ");
+            int number = int.Parse(Console.ReadLine());
+            Console.Write("Date (dd/mm/yyyy): ");
+            DateTime date = DateTime.Parse(Console.ReadLine());
+            Console.Write("Contract value: ");
+            double value = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+          
+            Console.Write("Enter number of installments: ");
+            int months = int.Parse(Console.ReadLine());
+            Contract myContract = new Contract(number, date, value);
+
+            ContractService contractService = new ContractService(new PaypalService());
+            contractService.ProcessContract(myContract, months);
+
+            Console.WriteLine("Installments:");
+            foreach (Installment installment in myContract.Installments)
+            {
+                Console.WriteLine(installment);
+
+
+            }
+        }
+    }
+}
---
+++ _14Exerc/Services/IOnlinePaymentService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _14Exerc.Services
+{
+    interface IOnlinePaymentService 
+    {
+        public double PaymentFree(double amount);
+
+        public double Interest(double amount, int mounths);
+    }
+}
